44e8c5af81003b663bcaa34437f17ee1b9aee697
Ghamdan-Aljbray <ghamdanaljbry2019>
2021-02-05 23:55:50 +0300

    Add function to Sense_Plane class

---
+++ Radar_Project/Classes/Sense_Plane.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Radar_Project.Classes
+{
+    class Sense_Plane 
+    {
+        static bool Is_plane = false;
+        static bool Radar_State;
+
+
+        /// <summary>
+        /// this is a function to Get Radar State 
+        /// </summary>
+        public static void Get_Radar_State()
+        {
+           // Radar_State = Start_Radar.Send_Radar_State();
+        }
+    }
+}
