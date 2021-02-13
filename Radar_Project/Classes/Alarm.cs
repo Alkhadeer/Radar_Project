0bc87c2e14b91b8b44f443ba969464308c312686
Rayaan1122 <rayaanalshawea>
2021-02-13 21:01:15 +0300

    Add Alarm Class

---
+++ Radar_Project/Classes/Alarm.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media;
+using System.Windows.Forms;
+using System.Drawing;
+namespace Ammar.Classes
+{
+    class Alarm : Location_Plane
+    {
+
+
+        public static void Message_for_User()
+        {
+            MessageBox.Show(" There is a plane in sky ", "Alarm ", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+
+    }
+}
