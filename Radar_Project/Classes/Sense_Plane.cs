897682c6ace99774583a5c8904fa21ec099c49fa
Ali-Alwaan <aliyahyaaliyahya2020>
2021-02-13 16:54:30 +0300

    Add function to Sense_Plane Class

--- Radar_Project/Classes/Sense_Plane.cs
+++ Radar_Project/Classes/Sense_Plane.cs
@@ -19,5 +19,24 @@ namespace Radar_Project.Classes
         {
            // Radar_State = Start_Radar.Send_Radar_State();
         }
+
+        /// <summary>
+        /// this is a function to sense Plane and return the result .
+        /// </summary>
+        /// <returns></returns>
+        public static bool Sense()
+        {
+            Get_Radar_State();
+
+            if (Form1.frm.Plane.Location.X == 1300)
+             //   Plane.PlainSound();
+
+            if (Radar_State)
+             //   if (X <= 1454 || Y <= 784)
+                {
+                    Is_plane = true;
+                }
+            return Is_plane;
+        }
     }
 }
