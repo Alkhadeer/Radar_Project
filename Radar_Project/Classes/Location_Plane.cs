7ffba24c500735bb17105a33f5c562799e43e92a
abdalghani123 <abd123456algny>
2021-02-13 17:16:57 +0300

    Add Property into Location_Plane Class

---
+++ Radar_Project/Classes/Location_Plane.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Ammar.Classes
+{
+    class Location_Plane
+    {
+
+        protected static int X;
+        protected static int Y;
+
+        /// <summary>
+        /// this is a property to get The Location of X axis of plane . 
+        /// </summary>
+        public int Get_X
+        {
+            get
+            {
+                return X;
+            }
+        }
+
+
+
+    }
+}
