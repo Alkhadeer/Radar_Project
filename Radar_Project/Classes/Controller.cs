3dd56e9a83ba060154bdad5f3eafcca4773900d6
Mohammedshafeeq385 <mmoohh385>
2021-02-13 20:51:07 +0300

    Add Controller Class

---
+++ Radar_Project/Classes/Controller.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media;
+namespace Ammar.Classes
+{
+    class Controller : Location_Plane
+    {
+
+        public static void Get_Location_Plane()
+        {
+            //if (Sense_Plane.Sense())
+            //{
+
+
+            //    if (Form1.frm.Plane.Location.X <= 610)
+            //    {
+            //        Classes.Defense.Lunch_Rocket();
+            //    }
+
+            //    if (Form1.frm.Plane.Location.X <= 500)
+            //    {
+            //        Form1.frm.Plane.Visible = false;
+            //        Form1.frm.label1.Visible = false;
+
+
+                }
+            }
+
+
+        }
+   
