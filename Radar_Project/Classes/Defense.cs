2aed55f43e628220b69b07ee1018c8f3a6be86b1
SADIQ12ALOTMI <Sadiqalotmy00>
2021-02-13 17:46:33 +0300

    Add Function into Defense Class

---
+++ Radar_Project/Classes/Defense.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media;
+using System.Windows.Forms;
+using System.Drawing;
+namespace Ammar.Classes
+{
+    class Defense : Location_Plane
+    {
+        public static SoundPlayer lunch = new SoundPlayer("lunch.wav");
+        public static SoundPlayer pom = new SoundPlayer("pom.wav");
+
+
+
+        /// <summary>
+        ///  this is a function to play sound of pom 
+        /// </summary>
+        public static void PomSound()
+        {
+            pom.Play();
+        }
+
+        /// <summary>
+        ///  this is a function to play sound of lunch 
+        /// </summary>
+        public static void lunchSound()
+        {
+
+            lunch.Play();
+        }
+    }
+}
