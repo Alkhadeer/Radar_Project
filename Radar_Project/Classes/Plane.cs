48fd42d7f5a2d788a18ccd2e6d1d6afb9ee652d9
Omar-Aljawfi7 <omar.aljawfi7>
2021-02-13 17:04:45 +0300

    Add Plane Class

---
+++ Radar_Project/Classes/Plane.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media;
+namespace Ammar.Classes
+{
+    class Plane //: Location_Plane
+    {
+        public static SoundPlayer plains = new SoundPlayer("PlainS.wav");
+
+
+        /// <summary>
+        ///  this is a function to play sound of plain
+        /// </summary>
+
+        public static void PlainSound()
+        {
+
+            plains.Play();
+        }
+
+    }
+}
